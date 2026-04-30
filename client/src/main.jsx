e8c3b5de8ad0622292689ad319e0972d081df3e5
Unnayan-Singh <unnayansingh2005@gmail.com>
2026-05-01 00:14:05 +0530

    Fixed router issue for Vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";  // 🔥 ADD THIS
+
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <HashRouter>   {/* 🔥 IMPORTANT */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </StrictMode>,
 )
